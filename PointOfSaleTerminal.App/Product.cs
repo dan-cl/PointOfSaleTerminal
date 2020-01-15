762693ef7e84edf3c1b53461e5181c6259d38e13
Dan-Cl <Dan.Clissold@protonmail.com>
2020-01-15 17:06:37 +1100

    Create Product Class

---
+++ PointOfSaleTerminal.App/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace PointOfSaleTerminal.App
+{
+    public class Product
+    {
+        public string ProductCode { get; }
+        public double UnitPrice { get; }   
+        public int PackSize { get; }
+        public double PackPrice { get; }
+
+        public Product(string productCode, double unitPrice)
+        {
+            ProductCode = productCode;
+            UnitPrice = unitPrice;
+        }
+
+        public Product(string productCode, double unitPrice, int packSize, double packPrice)
+        {
+            ProductCode = productCode;
+            UnitPrice = unitPrice;
+            PackSize = packSize;
+            PackPrice = packPrice;
+        }
+    }
+}
