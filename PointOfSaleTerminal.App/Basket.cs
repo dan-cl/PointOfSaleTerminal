701385e7f9322afd31b132bd94cc18aeea9cc5b5
Dan-Cl <Dan.Clissold@protonmail.com>
2020-01-15 17:24:40 +1100

    Create Basket and Basket Item classes

---
+++ PointOfSaleTerminal.App/Basket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace PointOfSaleTerminal.App
+{
+    public class Basket
+    {
+        public List<BasketItem> BasketItems { get; private set; }
+        public double BasketTotal { get; private set; }
+
+    }
+}
---
+++ PointOfSaleTerminal.App/BasketItem.cs
@@ -0,0 +1,13 @@
+﻿namespace PointOfSaleTerminal.App
+{
+    public class BasketItem
+    {
+        public Product Product { get; private set; }
+        public int Quantity { get; private set; }
+
+        public BasketItem(Product product)
+        {
+            Product = product;
+        }
+    }
+}
